29a7f43534f81fd2433586f47e6e13770b4d6f8c
Tanaka Yukio <news7vip@gmail.com>
2015-02-01 20:46:28 +0900

    First commit
    None

---
+++ SilentExplorer.Library.SilentExplorerCore/Data/Thread.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilentExplorer.Library.SilentExplorerCore.Data
+{
+    public class Thread
+    {
+        public string ThreadName { get; set; }
+
+        public string ThreadDatNumber { get; set; }
+
+        public DateTime ThreadStandTime
+        {
+            get
+            {
+                try
+                {
+                    var dt = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+                    dt.AddHours(9);
+                    return dt.AddSeconds(double.Parse(ThreadDatNumber));
+                }
+                catch
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+
+        public int ThreadCount { get; set; }
+
+        public int Index { get; set; }
+
+        public int Speed { get; set; }
+
+        public bool IsReaded { get; set; }
+
+
+    }
+}
---
+++ SilentExplorer.Windows.ViewModel/ThreadListViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using SilentExplorer.Library.SilentExplorerCore.Data;
+using System.Reactive;
+
+namespace SilentExplorer.Windows.ViewModel
+{
+    public class ThreadListViewModel : ReactiveObject
+    {
+        private ObservableCollection<Thread> hasThreads;
+
+        public ObservableCollection<Thread> Threads
+        {
+            get { return hasThreads; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref hasThreads, value);
+                hasThreads = value;
+            }
+        }
+
+
+        public ReactiveCommand<Unit> initialize;
+
+        public IReactiveCommand Initialize
+        {
+            get
+            {
+                if (initialize == null)
+                    initialize = ReactiveCommand.CreateAsyncTask(Initializer);
+                return initialize;
+            }
+        }
+
+        private async Task Initializer(object url)
+        {
+            var uri = url.ToString();
+            var threads = 
+        }
+    }
+}
